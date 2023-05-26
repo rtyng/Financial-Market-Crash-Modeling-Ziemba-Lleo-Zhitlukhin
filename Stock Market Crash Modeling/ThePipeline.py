c8a3e01d1604aa08886b76d3d1b697b662c916b5
rt <randytyng21@gmail.com>
2023-05-26 04:49:17 -0400

    Financial Crash Models
    Using Robert Shiller's S&P composite data from 1871 to present, will be implementing financial crash models on these data

---
+++ Stock Market Crash Modeling/ThePipeline.py
@@ -0,0 +1,13 @@
+# This script is a data pipeline. Shiller's Stock Market data 1871-present w/CAPE ratio
+
+import pandas as pd
+import numpy as np 
+import matplotlib as mpl 
+
+
+"""
+Part I
+Data loading, cleaning, inspection, deconstruction and reconstruction
+"""
+
+
