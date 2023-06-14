a00afbc866d7c4fe48ae1f2ce850ffbbf8d87227
rt <randytyng21@gmail.com>
2023-06-13 21:37:23 -0400

    Attempt at data cleaning
    Gonna need to check pandas documentation for how to best clean and process time series data for accurate analysis

--- Stock Market Crash Modeling/DataProcessing.py
+++ Stock Market Crash Modeling/DataProcessing.py
@@ -1,18 +1,23 @@
 # This script is using Shiller's Stock Market data 1871-present w/CAPE ratio
-
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-# load chapt26copy.xlsx, keep original for reference materials in sheet
+# load chapt26copy.xlsx, keep original for reference materials in sheets
+noninflated_df = pd.read_excel('chapt26copy.xlsx')
 
-# need to split excel data sheet in two
-    # theres 2 time columns to seperate the inflation adjusted variables from the non-inflation adjusted variables
-chapt_df = pd.read_excel("chapt26copy.xlsx", header=2, skiprows=7, names=None)
+# Extract the year values from the first column
+years = noninflated_df.iloc[:, 0]
 
+# Convert the years column to a datetime format
+noninflated_df['Year'] = pd.to_datetime(years, format='%Y')
 
-# data processing, cleaning, formatting, etc.
-print(chapt_df.head())
+# Set the 'Year' column as the index
+noninflated_df.set_index('Year', inplace=True)
 
+# Remove the original years column
+noninflated_df.drop(columns=noninflated_df.columns[0], inplace=True)
 
+# Check df
+print(noninflated_df.head())
 
